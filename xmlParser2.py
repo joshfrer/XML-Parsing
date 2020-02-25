cf733e846c31130ec2b3cd8aad9550d249bda5e7
joshfrer <61246580+joshfrer@users.noreply.github.com>
2020-02-25 12:48:42 -0700

    Update xmlParser2.py
    Here I am adding some functions to allow user to change and manipulate. Not finished by any means. I want to be able to store the xml as an object and pass it between functions.

--- xmlParser2.py
+++ xmlParser2.py
@@ -2,11 +2,15 @@
 Josh's XML Parser
 1/23/2020
 
-Takes in an XML file and puts each line into a dictionary.
 """
 
 import xml.etree.ElementTree as ET
-        
+
+
+def take_in_file():
+    # get_xml = input("Please copy and paste the xml file and input here: ")
+    pass
+
 def parseXML():
     # xml = input("Please copy and paste the xml file and input here: ")
     xml = "C:\\Users\\joshua.frerichs\\Desktop\\PASSED_ESYNT19014_83_276_2020_1_8_18_28.xml"
@@ -14,26 +18,43 @@ def parseXML():
     # this is the root
     testsuites = tree.getroot()
     # this is the child.
-    """ tag = input("What is the name of your new tag? ")
-    #tagText = input("What do you want the tag to say? ") """
     print(testsuites.tag, testsuites.attrib) 
     for testsuite in testsuites:
         print(testsuite.tag, testsuite.attrib)
-        """ testsuite.set(tag, tagText) """
-
-        """ find = input("What do you want to remove in testsuites? ")
-        for testsuite in testsuites.findall(find)
-        #testsuites.remove(testsuite) """
-
         # this is the child of the child
         for testcase in testsuite:
             print(testcase.tag, testcase.attrib)
-    
+            
+parseXML()
+
+def change_testsuites():
+    """ find = input("What do you want to remove in testsuites? ")
+        for testsuite in testsuites.findall(find)
+        #testsuites.remove(testsuite) """
+    """ tag = input("What is the name of your new tag? ")
+    #tagText = input("What do you want the tag to say? ") """
+    pass
+
+def change_testsuite():
+    pass
+
+def change_testcase():
+    pass
+
+def save_xml():
     """ yesNo = input("Would you like to save to the original file? (yes/no)")
     if yesNo == "yes":
-        tree.write(xml)
+        tree.write(get_xml)
     else:
         newFileName = input("Please enter new file name: ") + ".xml"
         tree.write(newFileName) """
-            
-parseXML()
+    pass
+
+def menu():
+    pass
+
+def if __name__ == "__main__":
+    pass
+
+
+
