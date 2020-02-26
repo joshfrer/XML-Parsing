bbb4f4c5aa2b30822defd44bf5e6eb4b66e5d1ae
joshfrer <61246580+joshfrer@users.noreply.github.com>
2020-02-25 18:40:27 -0700

    Update xmlParser2.py
    Here a feature menu has been added to read, parse, save, and quit menu.

--- xmlParser2.py
+++ xmlParser2.py
@@ -6,15 +6,13 @@ Josh's XML Parser
 
 import xml.etree.ElementTree as ET
 
-
 def take_in_file():
-    # get_xml = input("Please copy and paste the xml file and input here: ")
-    pass
+    get_xml = input("Please copy and paste the xml file and input here: ")
+    return get_xml
 
-def parseXML():
-    # xml = input("Please copy and paste the xml file and input here: ")
-    xml = "C:\\Users\\joshua.frerichs\\Desktop\\PASSED_ESYNT19014_83_276_2020_1_8_18_28.xml"
-    tree = ET.parse(xml)
+def parseXML(get_xml):
+    #xml = "C:\\Users\\joshua.frerichs\\Desktop\\PASSED_ESYNT19014_83_276_2020_1_8_18_28.xml"
+    tree = ET.parse(get_xml)
     # this is the root
     testsuites = tree.getroot()
     # this is the child.
@@ -24,8 +22,9 @@ def parseXML():
         # this is the child of the child
         for testcase in testsuite:
             print(testcase.tag, testcase.attrib)
-            
-parseXML()
+
+
+    return tree
 
 def change_testsuites():
     """ find = input("What do you want to remove in testsuites? ")
@@ -41,20 +40,43 @@ def change_testsuite():
 def change_testcase():
     pass
 
-def save_xml():
-    """ yesNo = input("Would you like to save to the original file? (yes/no)")
+def save_xml(tree, get_xml):
+    yesNo = input("Would you like to save to the original file name? (yes/no): ")
     if yesNo == "yes":
         tree.write(get_xml)
-    else:
+    elif yesNo == "no":
         newFileName = input("Please enter new file name: ") + ".xml"
-        tree.write(newFileName) """
-    pass
+        tree.write(newFileName)
+
 
 def menu():
-    pass
+    
+    trueFalse = False
+    print("To read a new file enter 'Read File'")
+    print("To parse current file enter 'Parse'")
+    print("To save your current file enter 'Save File'")
+    print("To quit the program enter 'Quit'")
+    while trueFalse != True:
+        option = input("Please enter a command: ")
+        if option == "Read File":
+            get_xml = take_in_file()
+        elif option == "Parse":
+            tree = parseXML(get_xml)
+        elif option == "Save File":
+            save_xml(tree, get_xml)
+        elif option == "Quit":
+            confirmation = input("Would you like to save before quitting? (yes/no): ")
+            if confirmation == "yes" and "Yes" and "y":
+                save_xml(tree, get_xml)
+                print("File has successfully saved!")
+                trueFalse = True
+            elif confirmation == "no" and "No" and "n":
+                print("Thank you!")
+                trueFalse = True
+                   
+    
+menu()
 
-def if __name__ == "__main__":
-    pass
-
-
+"""def if __name__ == "__main__":
+    pass"""
 
